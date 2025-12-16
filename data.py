f20a543efab8e3a019cca84196fbe8d9cc997c28
Alireza Kazemifard <110717590+Alireza-Kazemifard@users.noreply.github.com>
2025-12-16 16:28:52 +0200

    Refactor image loading and resizing logic

--- data.py
+++ data.py
@@ -1,15 +1,19 @@
+# --- START OF FILE data.py ---
 import os
 import numpy as np
 import cv2
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
+from glob import glob
 
-IMAGE_HEIGHT = 256
-IMAGE_WIDTH = 256
+
+IMAGE_HEIGHT = 224
+IMAGE_WIDTH = 224
 
 def load_data(dataset_path, split=0.1):
-    images = sorted([os.path.join(dataset_path, "images", x) for x in os.listdir(os.path.join(dataset_path, "images"))])
-    masks = sorted([os.path.join(dataset_path, "masks", x) for x in os.listdir(os.path.join(dataset_path, "masks"))])
+    
+    images = sorted(glob(os.path.join(dataset_path, "images", "*")))
+    masks = sorted(glob(os.path.join(dataset_path, "masks", "*")))
     
     # Split
     train_x, test_x, train_y, test_y = train_test_split(images, masks, test_size=split, random_state=42)
@@ -20,16 +24,19 @@ def load_data(dataset_path, split=0.1):
 def read_image(path):
     path = path.decode()
     x = cv2.imread(path, cv2.IMREAD_COLOR)
-    x = cv2.resize(x, (IMAGE_WIDTH, IMAGE_HEIGHT))
+    
+    x = cv2.resize(x, (IMAGE_WIDTH, IMAGE_HEIGHT), interpolation=cv2.INTER_LANCZOS4)
     x = x / 255.0
     return x.astype(np.float32)
 
 def read_mask(path):
     path = path.decode()
     x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    x = cv2.resize(x, (IMAGE_WIDTH, IMAGE_HEIGHT))
+    
+    x = cv2.resize(x, (IMAGE_WIDTH, IMAGE_HEIGHT), interpolation=cv2.INTER_LANCZOS4) 
     x = x / 255.0
     x = np.expand_dims(x, axis=-1)
+    
     x = np.where(x > 0.5, 1.0, 0.0)
     return x.astype(np.float32)
 
@@ -45,13 +52,12 @@ def tf_parse(x, y):
 
 def tf_dataset(X, Y, batch_size=8, augment=False):
     dataset = tf.data.Dataset.from_tensor_slices((X, Y))
-    dataset = dataset.map(tf_parse)
+    dataset = dataset.map(tf_parse, num_parallel_calls=tf.data.AUTOTUNE)
     
     if augment:
-        # Simple Augmentation using TF
         dataset = dataset.map(lambda x, y: (tf.image.random_flip_left_right(x), tf.image.random_flip_left_right(y)))
         dataset = dataset.map(lambda x, y: (tf.image.random_flip_up_down(x), tf.image.random_flip_up_down(y)))
         
     dataset = dataset.batch(batch_size)
-    dataset = dataset.prefetch(1)
+    dataset = dataset.prefetch(tf.data.AUTOTUNE)
     return dataset
