f0f6b74cf400c710b74c45146b9eee1c06c9679f
Alireza Kazemifard <110717590+Alireza-Kazemifard@users.noreply.github.com>
2025-12-16 21:37:27 +0200

    Clean up load_trained_model function
    Removed commented line about adding metrics to CustomObjectScope and a print statement for successful model loading.

--- utils.py
+++ utils.py
@@ -13,7 +13,6 @@ def create_directory(directory_path):
         print(f"Error: creating directory with name {directory_path}")
 
 def load_trained_model(model_path):
-    # اضافه کردن تمام متریک‌ها به CustomObjectScope برای جلوگیری از ارور هنگام لود مدل
     with CustomObjectScope({
             'intersection_over_union': intersection_over_union,
             'dice_coefficient': dice_coefficient,
@@ -25,5 +24,4 @@ def load_trained_model(model_path):
             'mean_absolute_error': mean_absolute_error
         }):
         loaded_model = tf.keras.models.load_model(model_path)
-        print("✅ Model loaded successfully.")
         return loaded_model
