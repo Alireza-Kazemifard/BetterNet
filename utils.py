8412ddbbb7c495f0a9c88b2d693aa6e23b6bb1b0
Alireza Kazemifard <110717590+Alireza-Kazemifard@users.noreply.github.com>
2025-12-16 16:06:10 +0200

    Enhance utils.py with data shuffling and model loading
    Added functions to shuffle data and load trained models with custom metrics.

--- utils.py
+++ utils.py
@@ -1,17 +1,32 @@
-
 import os
+import json
+from sklearn.utils import shuffle
 import tensorflow as tf
-from metrics import *
+from tensorflow.keras.utils import CustomObjectScope
+from tensorflow.keras.metrics import Recall, Precision, MeanIoU
+from tensorflow.keras.optimizers import Adam
+from metrics import intersection_over_union, dice_coefficient, dice_loss, binary_crossentropy_dice_loss
 
-def create_directory(path):
-    if not os.path.exists(path): os.makedirs(path)
-
-def load_model(model_path):
-    custom_objects = {
-        'dice_loss': dice_loss, 'dice_coefficient': dice_coefficient, 
-        'bce_dice_loss': bce_dice_loss, 'binary_crossentropy_dice_loss': bce_dice_loss
-    }
+def create_directory(directory_path):
+    """ Create a directory. """
     try:
-        return tf.keras.models.load_model(model_path, custom_objects=custom_objects, safe_mode=False)
-    except:
-        return tf.keras.models.load_model(model_path, custom_objects=custom_objects, compile=False)
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+    except OSError:
+        print(f"Error: creating directory with name {directory_path}")
+
+def shuffle_data(features, labels):
+    """ Shuffle the data. """
+    shuffled_features, shuffled_labels = shuffle(features, labels, random_state=42)
+    return shuffled_features, shuffled_labels
+
+def load_trained_model(model_path):
+    """ Load the saved model with custom metrics/losses. """
+    with CustomObjectScope({
+            'intersection_over_union': intersection_over_union,
+            'dice_coefficient': dice_coefficient,
+            'dice_loss': dice_loss,
+            'binary_crossentropy_dice_loss': binary_crossentropy_dice_loss
+        }):
+        loaded_model = tf.keras.models.load_model(model_path)
+        return loaded_model
